d20a2674942a39e0e842d619112fbe0cab6c79f4
Devontae <s2kdevelopshare@gmail.com>
2025-03-11 11:27:40 +1300

    ReadMe Initial

--- JudgeFrontend/src/components/modals/SubmissionModal.tsx
+++ JudgeFrontend/src/components/modals/SubmissionModal.tsx
@@ -1,5 +1,7 @@
 import { Dialog } from "@headlessui/react";
 
+// TODO: Implement selects for language, lab, paper
+
 export default function SubmissionModal({
     isOpen,
     setIsOpen,
@@ -14,8 +16,9 @@ export default function SubmissionModal({
             className="fixed inset-0 flex items-center justify-center"
         >
             <div className="fixed inset-0 bg-black opacity-50"></div>
-            <Dialog.Panel className="bg-white p-6 rounded shadow-lg z-10">
+            <Dialog.Panel className="bg-white min-w-96 p-6 rounded shadow-lg z-10">
                 <h1 className="text-2xl font-medium mb-4">Lab Submission</h1>
+                <p>Label</p>
                 <button onClick={() => setIsOpen(false)}>Cancel</button>
             </Dialog.Panel>
         </Dialog>
